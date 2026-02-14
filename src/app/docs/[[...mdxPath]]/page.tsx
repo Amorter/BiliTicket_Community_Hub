d38265a95315eb3e25b71f902b90d7e289ae5fef
Amort <a1145252643@gmail.com>
2026-02-15 06:09:07 +0800

    修复 useMDXComponents 的调用方式以确保正确获取 Wrapper 组件

--- src/app/docs/[[...mdxPath]]/page.tsx
+++ src/app/docs/[[...mdxPath]]/page.tsx
@@ -11,7 +11,7 @@ export async function generateMetadata(props: {
   return metadata
 }
 
-const Wrapper = useMDXComponents().wrapper
+const Wrapper = useMDXComponents({}).wrapper
 
 export default async function Page(props: {
   params: Promise<{ mdxPath?: string[] }>
